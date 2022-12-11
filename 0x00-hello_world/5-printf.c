4cdc26d87747763ec53f295834d92685fc85d0c0
natantaddele <taddele@gmail.com>
2022-12-11 14:32:56 +0300

    Task4-8

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,3 @@
-
-
 #include <stdio.h>
 /**
  * main - print a c program using printf
@@ -7,6 +5,7 @@
 */
 
 int main(void)
+{
 
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
