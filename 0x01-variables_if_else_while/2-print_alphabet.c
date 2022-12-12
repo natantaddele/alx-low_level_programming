78570324f31537ae3df2f68a0523f3d23b8041c0
natantaddele <taddele@gmail.com>
2022-12-12 22:43:26 +0300

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -5,13 +5,13 @@
  */
 int main(void)
 {
-    char alp[26] = "abcdefjhigklmnopqrstuvwxyz";
-    int i;
+ char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+ int i;
 
-    for (i = 0; i < 26; i++)
+ for (i = 0; i < 26; i++)
     {
         putchar(alp[i]);
     }
-    putchar('\n');
-    return (0);
+ putchar('\n');
+ return (0);
 }
