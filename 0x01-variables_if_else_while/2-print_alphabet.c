d88d7c9a9d8b01f9e1a844ee8554a6b3e0e6e743
natantaddele <taddele@gmail.com>
2022-12-12 23:11:39 +0300

    sometime suffer from isomania

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 /**
- * man - Prints the alphabet. 
+ * man - Prints the alphabet.
  * Return: Always 0 (Success)
  */
 int main(void)
-{ 
+{
 char alp[26] = "abcdefghijklmnopqrstuvwxyz";
 int i;
- 
+
 for (i = 0; i < 26; i++)
     {
         putchar(alp[i]);
