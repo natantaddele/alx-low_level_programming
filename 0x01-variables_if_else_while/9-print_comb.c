c5ce2141732ba3cfba61bda0f56fddbac358378e
natantaddele <taddele@gmail.com>
2022-12-13 01:53:26 +0300

    possible-combination

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,18 +8,17 @@
 
 int main(void)
 {
-    int d;
+int d;
 
-    for (d = '0'; d <= '9'; d++)
-    {
-    putchar(d);
-    if (d != '9')
-    {
-    putchar(',');
-    putchar(',');
-    }
-    }
-    putchar('\n');
-    return (0);
+for (d = '0'; d <= '9'; d++)
+{
+putchar(d);
+if (d != '9')
+{
+putchar(',');
+}
+}
+putchar('\n');
+return (0);
     
 }
