b2aa2bbba980a3a2290ab3eeb24bb084c7f00724
natantaddele <taddele@gmail.com>
2022-12-12 23:33:46 +0300

    some time suffer from isomania

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -9,9 +9,9 @@ char alp[26] = "abcdefghijklmnopqrstuvwxyz";
 int i;
 
 for (i = 0; i < 26; i++)
-    {
-        putchar(alp[i]);
-    }
+       {
+putchar(alp[i]);
+       }
 putchar('\n');
 return (0);
 }
