7876ab262ace4ee8d3b4b5ff77c155bd96ddf0e6
natantaddele <taddele@gmail.com>
2022-12-13 22:00:09 +0300

    Task-0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "quantum.h"
+
+/**
+*main - printer putchar
+*Return: 0 always
+*/
+int main(void)
+{
+
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('_');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+
+return (0);
+}
