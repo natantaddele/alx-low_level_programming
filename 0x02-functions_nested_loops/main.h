c780af735993a436f0c0d9da0d6e57a2081d98ee
natantaddele <taddele@gmail.com>
2022-12-14 20:45:56 +0300

    9. Learn your times table

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -9,10 +9,10 @@ int _isalpha(int c);
 int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
-void jach_bauer(void);
+void jack_bauer(void);
 int add(int a, int b);
 void print_to_98(int n);
-void times_table(int n);
+void times_table(void);
 void num(int r, int n);
 
 #endif
