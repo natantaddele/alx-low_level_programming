792dde2bd9eea7970e748d0ae82c2b939eecc337
natantaddele <taddele@gmail.com>
2022-12-14 22:01:40 +0300

    8. I'm federal agent Jack Bauer, and today is the longest day of my life

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,19 +6,26 @@
  */
 void jach_bauer(void)
 {
-int a;
-int b;
-
-for (a = 0; a <= 23; a++)
+int a, b, c, d;
+for (a = 0; a <= 2; a++)
 {
-for (b = 0; a <= 59; b++)
+for (b = 0; a <= 9; b++)
 {
-_putchar (a / 10 + '0');
-_putchar (a % 10 + '0');
-_putchar (':');
-_putchar (b / 10 + '0');
-_putchar (b % 10 + '0');
+if ((a <= 1 && b <= 9) || (a <= 2 && b <=3))
+{
+for (c = 0; c <= 5; c++)
+{
+for (d = 0; d <= 0; d++)
+{
+_putchar (a + '0');
+_putchar (b + '0');
+_putchar (58);
+_putchar (c + '0');
+_putchar (d + '0');
 _putchar ('\n');
+}
+}
+}
 }
 }
 }
