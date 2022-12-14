7a2f4d166ce3ebb4cd32ebcc0c1ef2b0ad71b9a1
natantaddele <taddele@gmail.com>
2022-12-14 23:08:24 +0300

    9. Learn your times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -28,7 +28,7 @@ _putchar(' ');
 _putchar('0' + rep / 10);
 _putchar('0' + rep % 10);
 }
-else
+if else
 {
 _putchar('0' + rep / 100);
 _putchar('0' + (rep - 100) / 10);
@@ -42,5 +42,6 @@ _putchar(' ');
 }
 _putchar('\n');
 a++;
+}
 }
 }
