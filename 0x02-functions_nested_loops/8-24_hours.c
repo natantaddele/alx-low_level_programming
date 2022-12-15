f851b432e7091e8174ad60a31cf4418e314b4566
natantaddele <taddele@gmail.com>
2022-12-15 11:50:46 +0300

    8-main_0.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,31 +1,22 @@
 #include "main.h"
 
 /**
- * Jach_Bauer - prints every minute of the day. 
+ * jach_bauer - prints every minute of the day. 
  * Return: Always 0.
  */
 void jach_bauer(void)
 {
-int a, b, c, d;
-for (a = 0; a <= 2; a++)
+int a, b;
+for (a = 0; a <= 23; a++)
 {
-for (b = 0; a <= 9; b++)
+for (b = 0; b <= 59; b++)
 {
-if ((a <= 1 && b <= 9) || (a <= 2 && b <=3))
-{
-for (c = 0; c <= 5; c++)
-{
-for (d = 0; d <= 0; d++)
-{
-_putchar (a + '0');
-_putchar (b + '0');
-_putchar (58);
-_putchar (c + '0');
-_putchar (d + '0');
+_putchar (a / 10 + '0');
+_putchar (a % 10 + '0');
+_putchar (':');
+_putchar (b / 10 + '0');
+_putchar (b % 10 + '0');
 _putchar ('\n');
-}
-}
-}
 }
 }
 }
