4d3b78b2e054e4a62ba4986491063dfc039cd7b1
natantaddele <taddele@gmail.com>
2022-12-15 14:30:52 +0300

    16.fibonacci

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -6,7 +6,6 @@
  */
 int main(void)
 {
-int i;
 unsigned long int i, j, k, j1, j2, k1, k2;
 j = 1;
 k = 2;
@@ -26,11 +25,11 @@ for (i = 92; i < 99; ++i)
 {
 printf(", %lu", k1 + (k2 / 1000000000));
 printf("%lu", k2 % 1000000000);
-k1 = k1 + ji;
+k1 = k1 + j1;
 j1 = k1 - j1;
 k2 = k2 + j2;
 j2 = k2 - j2;
 }
-printf('\n');
+printf("\n");
 return (0);
 }
