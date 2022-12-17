c9fe885b61cb5c4fa072e44519034fd5700e037a
natantaddele <taddele@gmail.com>
2022-12-17 03:25:21 +0300

    0. isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,15 +3,14 @@
 /**
 * _ispper - check if a letter is upper
 * @c: The number to be checked
-* Return: 1 for upper letter or 0 for any else
+*
+* Return: 0 or 1
 */
+
 int _isupper(int c)
 {
 if (c >= 'A' && c <= 'Z')
-{
 return (1);
-}
 else
-{
 return (0);
 }
