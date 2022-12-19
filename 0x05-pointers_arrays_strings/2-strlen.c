4aecdb9210355e244189f9dbb99e212d953a16b4
natantaddele <taddele@gmail.com>
2022-12-19 22:28:59 +0300

    2. This report, by its very length, defends itself against the risk of being read

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -8,8 +8,11 @@
  * Return: Length of the string 
  */
 int _strlen(char *s)
+{
+
 int index;
 
-for (index = 0; s[index] != '0' ; index++)
+for (index = 0; s[index] != '\0' ; index++)
 
 return (index);
+}
