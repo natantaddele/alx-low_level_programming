a32e46fce5039eb20a142de8fb2a8a5dae208e1e
natantaddele <taddele@gmail.com>
2022-12-19 22:19:28 +0300

    2. This report, by its very length, defends itself against the risk of being read

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "mian.h"
+
+/**
+ * _strlen - Function that returns the length
+ *
+ * @s: this is thte input
+ *
+ * Return: Length of the string 
+ */
+int _strlen(char *s)
+int index;
+
+for (index = 0; s[index] != '0' ; index++)
+
+return (index);
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,7 +1,9 @@
 #ifndef MAIN_H                                                 
 #define MAIN_H                                                 
                                                                
-#include <stdio.h>                                             
+#include <stdio.h>
+
+int_srlen(char *s);
 int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
