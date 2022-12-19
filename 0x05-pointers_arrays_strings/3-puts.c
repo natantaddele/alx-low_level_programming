28198e338dcb02d4315136c9c3c453506f99040f
natantaddele <taddele@gmail.com>
2022-12-19 22:52:44 +0300

    2. This report, by its very length, defends itself against the risk of being read

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,11 +1,8 @@
 #include "main.h"
-
 /**
- *_puts - write function that prints a string
- *
- * @str: this is my input string 
+ * _puts - write function that prints a string
+ * @str:this is my input string 
  */
-
 void _puts(char *str)
 {
 int index;
