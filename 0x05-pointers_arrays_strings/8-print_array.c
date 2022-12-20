5b59222d5214f1b9b3a56e7328adc89d00158fca
natantaddele <taddele@gmail.com>
2022-12-20 23:52:18 +0300

    Task-8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,5 +1,22 @@
 #include "main.h"
 
 /**
- * :x
+ * print_array - unctin that prints n elements of an array o integers
  *
+ * @a: pointer to first int 
+ * @n: p0inter to second int
+ * Rerurn: nothing
+ */
+
+void print_array(int *a, int n)
+{
+	int i = 0;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+		if (i < n - 1)
+			printf(", ");
+	}
+	printf("\n");
+}
