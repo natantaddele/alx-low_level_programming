f061062a39b88bfa81d7b97a0e02670b61d60842
natantaddele <taddele@gmail.com>
2022-12-21 00:02:21 +0300

    Task-9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strcpy - function that copies the string
+ *
+ * @dest: pinter to destination char
+ * @src: pinter to source char
+ * Return: char
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+	
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		*(dest + i) = *(src +);
+	}
+	*(dest + i) = '\0';
+	return (dest);
+}
