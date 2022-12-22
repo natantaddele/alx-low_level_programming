9700142a808c91974cf630b09f0f898738044cfb
natantaddele <taddele@gmail.com>
2022-12-22 14:20:18 +0300

    Task-0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,7 +2,7 @@
 
 /**
  * strcat - Concatenates the string pointed to by @src, including the x
- *          null byte, to the end of the string pointed to by @dest.
+ * null byte, to the end of the string pointed to by @dest.
  * @dest: A pointer to the string to be concatenated upon.
  * @src: The source string to be appended to @dest.
  *
@@ -11,24 +11,23 @@
 
 char str1[100] = "Geeks", str2[100] = "World";
 {
-    char str3[100];
+char str3[100];
 
-    int i = 0, j = 0;
+int i = 0, j = 0;
 
-    while (str1[i] != '\0')
-    {
-        str3[j] = str1[i];
-        i++;
-        j++;
-    }
-    while (str2[i] != '\0')
-    {
-        str3[j] = str2[i];
-        i++;
-        j++;
-    }
-    str3[j] = '\0';
-    _putchar("\nConcatenated string: %s", str3);
-
-    return 0;
+while (str1[i] != '\0')
+{
+str3[j] = str1[i];
+i++;
+j++;
+}
+while (str2[i] != '\0')
+{
+str3[j] = str2[i];
+i++;
+j++;
+}
+str3[j] = '\0';
+_putchar("\nConcatenated string: %s", str3);
+return (0);
 }
