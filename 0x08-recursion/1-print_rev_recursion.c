875c817f378dc155ffdd03e50c1b21d3f864730a
natantaddele <taddele@gmail.com>
2023-01-04 21:37:11 +0300

    Task-2

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 
 /**
- * _rev_recursion - Print string revers.
- * 
+ * _print_rev_recursion - Print string revers.
  * @s: Input Variable.
  *
  * Return: Always 0.
@@ -13,7 +12,6 @@ void _print_rev_recursion(char *s)
 if (*s == '\0')
 
 {
-	
 return;
 }
 _print_rev_recursion(s + 1);
