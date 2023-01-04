b5c85b2de4396467721802f21847f3ae3d8315e7
natantaddele <taddele@gmail.com>
2023-01-04 21:16:46 +0300

    Task-1

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,14 +2,18 @@
 
 /**
  * _puts_recursion - check the code
+ *
  * @s: its variable 
  *
  * Return: Always 0.
  */
 
-void _puts_recursion(char *s){
+void _puts_recursion(char *s)
 
-if (*s == '\0') {
+{
+
+if (*s == '\0') 
+{
 _putchar('\n');
 return;
 }
