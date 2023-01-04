2db92e16ab056876fb255e5df65718d0c38726dd
natantaddele <taddele@gmail.com>
2023-01-04 22:34:22 +0300

    Task-5

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * _pow_recursion - Print The power of I.
+ * @y: First variable
+ * @x: Input Variable.
+ *
+ * Return: Always 0.
+ */
+int _pow_recursion(int x, int y)
+
+{
+if (y < 0)
+
+{
+return (-1);
+}
+if (y == 0)
+
+{
+return (1);
+}
+return (x * _pow_recursion(x, y - 1));
+}
