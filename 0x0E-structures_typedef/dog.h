088b6f5a365f4b892e6fb3ce3c0a6ef93483b3df
natantaddele <taddele@gmail.com>
2023-01-16 22:50:46 +0300

    poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,7 @@
 #ifndef DOG_H
 #define DOG_H
 
+
 /**
  * struct dog - struct dog
  * @name: name
@@ -25,3 +26,5 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
+
+#endif
