d1535da9860df7543061f5440a614e78f2b5aaf4
natantaddele <taddele@gmail.com>
2023-01-19 14:54:38 +0300

    0.What is my name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -2,8 +2,9 @@
 #include "function_pointers.h"
 
 /**
- * print_name_as_is - prints a name as is 
- * @name: name of the person
+ * print_name - prints a name as is
+ *
+ * @f: name of the person
  *
  * Return: Nothing.
  */
